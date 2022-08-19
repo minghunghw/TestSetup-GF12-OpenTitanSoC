e095278b9992be753dbc9cd074bdff1fd53f3f70
Ming-Hung Chen <minghung@umich.edu>
2022-08-19 13:51:30 -0400

    Fix typo and read feature

--- scripts/find_usb_addr.py
+++ scripts/find_usb_addr.py
@@ -14,7 +14,7 @@ from pyftdi.usbtools import UsbTools
 gpio = []
 gpio_addr = []
 FT232H_list = UsbTools.find_all([(0x403, 0x6014)])
-print("Found " + len(FT232H_list) + " devices")
+print("Found " + str(len(FT232H_list)) + " devices")
 for i in range(len(FT232H_list)):
     gpio_addr.append("ftdi://ftdi:232h:" + hex(FT232H_list[i][0].bus)[2:].zfill(2) + ":"+hex(FT232H_list[i][0].address)[2:].zfill(2)+"/1")
     print("USB Address of the " + str(i) + " board to be checked is: " + gpio_addr[i])
@@ -25,14 +25,27 @@ for i in range(len(gpio_addr)):
     # direction = C[7:0], D[7:0]
     gpio[i].configure(gpio_addr[i], direction=0x00ff, frequency=1e6)
 
-idex = input("Which board do you want to check?")
-while true:
+#index = input("Which board do you want to check?")
+index = 1
+while True:
     # Write D0 to 1
     gpio[index].write(0x0001)
+    # Read whole GPIOs
+    val = gpio[index].read()[0]
+    print(f"{val:016b}")
+    # Read C0 from D0
+    val = (val >> 8) & 0b1
+    print(val)
     # Sleep for 1 second
     time.sleep(1)
     # Write D0 to 0
     gpio[index].write(0x0000)
+    # Read whole GPIOs
+    val = gpio[index].read()[0]
+    print(f"{val:016b}")
+    # Read C0 from D0
+    val = (val >> 8) & 0b1
+    print(val)
     # Sleep for 1 second
     time.sleep(1)
 
